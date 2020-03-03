118d3a68ab50a087c1b1747e97e5c64f804248c8
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-03-04 03:44:55 +0900

    Tweaked copyToClipboard implementation

--- src/components/App/helpers/copyToClipboard.js
+++ src/components/App/helpers/copyToClipboard.js
@@ -1,37 +1,29 @@
-const copyToClipboard = text => {
-	let fakeElem = document.createElement("textarea")
-	fakeElem.style.fontSize = "12pt"
-	fakeElem.style.border = "0"
-	fakeElem.style.padding = "0"
-	fakeElem.style.margin = "0"
-	fakeElem.style.position = "absolute"
-	fakeElem.style.left = "-9999px"
-
-	const yPosition = window.pageYOffset || document.documentElement.scrollTop
-	fakeElem.style.top = `${yPosition}px`
-
-	fakeElem.setAttribute("readonly", "")
-	fakeElem.value = text
-
-	document.body.appendChild(fakeElem)
-
-	if (isIOS()) {
-		const range = document.createRange()
-		const selection = window.getSelection()
-		range.selectNodeContents(fakeElem)
-		selection.removeAllRanges()
-		selection.addRange(range)
-		fakeElem.setSelectionRange(0, 999999)
-	} else {
-		fakeElem.select()
-	}
-
-	document.execCommand("copy")
-
-	document.body.removeChild(fakeElem)
-	fakeElem = null
+function testIOS() {
+	return navigator.userAgent.match(/ipad|iphone/i)
 }
 
-const isIOS = () => navigator.userAgent.match(/ipad|iphone/i)
+// https://github.com/codex-src/heroicons-viewer/pull/4
+function copyToClipboard(data) {
+	const textarea = document.createElement("textarea")
+	textarea.style.position = "absolute" // eslint-disable-line no-multi-spaces
+	textarea.style.left     = "-9999px"  // eslint-disable-line no-multi-spaces
+	textarea.style.top      = "-9999px"  // eslint-disable-line no-multi-spaces
+	textarea.value          = data       // eslint-disable-line no-multi-spaces
+	document.body.appendChild(textarea)
+	const selection = document.getSelection()
+	if (testIOS()) {
+		// const selection = document.getSelection()
+		const range = document.createRange()
+		range.selectNodeContents(textarea)
+		selection.removeAllRanges()
+		selection.addRange(range)
+		textarea.setSelectionRange(0, textarea.value.length)
+	} else {
+		textarea.select()
+	}
+	document.execCommand("copy")
+	selection.removeAllRanges()
+	document.body.removeChild(textarea)
+}
 
 export default copyToClipboard
