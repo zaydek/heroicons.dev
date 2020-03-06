ca43a6107d4d91f67e35b444c5ab124569b3bd02
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-03-07 08:29:06 +0900

    Added a favicon

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import * as serviceWorker from "./serviceWorker"
 import App from "./components/App"
 import React from "react"
 import ReactDOM from "react-dom"
+
+import "__firebase"
 
 import "debug.css"
 import "stylesheets/reset-input.css"
 import "stylesheets/tailwind.generated.css"
 
 ReactDOM.render(<App />, document.getElementById("root"))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
