7470817bc0d94d9d62c36c40ed1a109637682ca1
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-04-03 19:33:35 +0900

    Added tag comments

--- src/App/helpers/icons.js
+++ src/App/helpers/icons.js
@@ -1,5 +1,41 @@
 import * as Hero from "react-heroicons"
 
+// "adjustments configure controls settings sliders",
+// "annotation box chat comment message square suggestion",
+// "archive bin",
+// "arrow circle direction down"
+// "arrow circle direction left"
+// "arrow circle direction right"
+// "arrow circle direction up"
+// "arrow circle direction down"
+// "arrow direction left"
+// "arrow direction down narrow"
+// "arrow direction left narrow"
+// "arrow direction narrow right"
+// "arrow direction narrow up"
+// "arrow direction right"
+// "arrow direction up"
+// "address at email symbol"
+// "alert bell notification",
+// "book bookmark open page read",
+// "book bookmark",
+// "bag briefcase business travel",
+// "calendar date time travel",
+// "camera photo photography travel",
+// "bill cash dollar money",
+// "business chart data math science",
+// "annotation bubble chat circle comment message suggestion",
+// "check circle complete done task todo",
+// "check complete done task todo",
+// "arrow cheveron chevron direction down",
+// "arrow cheveron chevron direction left",
+// "arrow cheveron chevron direction right",
+// "arrow cheveron chevron direction up",
+// "check clipboard copy done paste task todo",
+// "check clipboard copy done paste task todo",
+// "check clipboard copy done paste task todo",
+// "check clipboard copy done paste task todo",
+
 const originalIcons = [
 	{
 		name:    "adjustments",
