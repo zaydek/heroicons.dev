812d323257a1dbc865feab0f10b9c36413eab35c
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-10 15:02:11 +0900

    Removed comments

--- src/App/svgToJSX.js
+++ src/App/svgToJSX.js
@@ -1,25 +1,16 @@
 // Converts SVG HTML to JSX.
 function svgToJSX(svgStr) {
 	let jsxStr = svgStr
-
-	// <(svg|path)></(svg|path)> -> <(svg|path) />
 	jsxStr = jsxStr.replace(/><\/[a-z]+>/g, " />")
-
-	// <svg class="hello world" ...> -> <svg className="hello world" ...>
 	jsxStr = jsxStr.replace(/class="/g, "className=\"")
-
-	// <(svg|path) [a-z]+-[a-z]+ ...> -> <(svg|path) [a-z]+[A-Z]+ ...>
 	jsxStr = jsxStr.replace(/fill-rule/g, "fillRule")
 	jsxStr = jsxStr.replace(/clip-rule/g, "clipRule")
 	jsxStr = jsxStr.replace(/stroke-linecap/g, "strokeLinecap")
 	jsxStr = jsxStr.replace(/stroke-linejoin/g, "strokeLinejoin")
 	jsxStr = jsxStr.replace(/stroke-width/g, "strokeWidth")
-
-	// <path strokeWidth="2" ...> -> <path strokeWidth={2} ...>
 	jsxStr = jsxStr.replace(/strokeWidth="(\d+)"/g, (_, strokeWidth) => {
 		return `strokeWidth={${strokeWidth}}`
 	})
-
 	return jsxStr
 }
 
