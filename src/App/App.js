8d94006dee6bb0a1a54b94b2fdf4948af1d9a6c6
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-13 18:53:38 +0900

    Better focus shadow for the sponsor button

--- src/App/App.js
+++ src/App/App.js
@@ -141,14 +141,14 @@ const App = () => {
 											</div>
 											<div className="h-4" />
 											<div className="px-2 w-full">
-												<button className="py-0.5 w-full bg-gray-700 border border-gray-600 shadow rounded">
-													<p className="flex flex-row justify-center items-center font-semibold text-md text-gray-100" style={{ fontSize: tw(3.75) }}>
+												<a className="py-1 inline-block w-full bg-gray-800 border border-gray-600 focus:border-transparent rounded focus:outline-none shadow focus:shadow-solid-indigo transition duration-200 ease-in-out" href="https://github.com/sponsors/codex-zaydek" {...attrs.target_blank}>
+													<p className="flex flex-row justify-center items-center font-semibold text-md leading-none text-gray-100" style={{ fontSize: tw(3.75) }}>
 														<svg className="mr-2 w-5 h-5 fill-current text-indigo-500 transform scale-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
 															<path fillRule="evenodd" d="M7.655 14.916L8 14.25l.345.666a.752.752 0 01-.69 0zm0 0L8 14.25l.345.666.002-.001.006-.003.018-.01a7.643 7.643 0 00.31-.17 22.08 22.08 0 003.433-2.414C13.956 10.731 16 8.35 16 5.5 16 2.836 13.914 1 11.75 1 10.203 1 8.847 1.802 8 3.02 7.153 1.802 5.797 1 4.25 1 2.086 1 0 2.836 0 5.5c0 2.85 2.045 5.231 3.885 6.818a22.075 22.075 0 003.744 2.584l.018.01.006.003h.002z" />
 														</svg>
 														Sponsor
 													</p>
-												</button>
+												</a>
 											</div>
 										</div>
 									</div>
--- src/tailwind-css/tailwind.config.js
+++ src/tailwind-css/tailwind.config.js
@@ -2,9 +2,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 const defaultVariants = require("./defaultVariants")
 
-function trimWhitespace(str) {
-	return str.split(/\s+/).join(" ").trim()
-}
+// function trimWhitespace(str) {
+// 	return str.split(/\s+/).join(" ").trim()
+// }
 
 module.exports = {
 	purge: [
@@ -17,34 +17,36 @@ module.exports = {
 				"75": "0.75rem",
 			},
 			boxShadow: {
-				"hero-sm": trimWhitespace(`
-					0 0 0 1px rgba(0, 0, 0, 0.05),
-					0 1px 2px 0 rgba(0, 0, 0, 0.05)
-				`),
-				"hero": trimWhitespace(`
-					0 0 0 1px rgba(0, 0, 0, 0.05),
-					0 1px 3px 0 rgba(0, 0, 0, 0.1),
-					0 1px 2px 0 rgba(0, 0, 0, 0.06)
-				`),
-				"hero-md": trimWhitespace(`
-					0 0 0 1px rgba(0, 0, 0, 0.05),
-					0 4px 6px -1px rgba(0, 0, 0, 0.1),
-					0 2px 4px -1px rgba(0, 0, 0, 0.06)
-				`),
-				"hero-lg": trimWhitespace(`
-					0 0 0 1px rgba(0, 0, 0, 0.05),
-					0 10px 15px -3px rgba(0, 0, 0, 0.1),
-					0 4px 6px -2px rgba(0, 0, 0, 0.05)
-				`),
-				"hero-xl": trimWhitespace(`
-					0 0 0 1px rgba(0, 0, 0, 0.05),
-					0 20px 25px -5px rgba(0, 0, 0, 0.1),
-					0 10px 10px -5px rgba(0, 0, 0, 0.04)
-				`),
-				"hero-2xl": trimWhitespace(`
-					0 0 0 1px rgba(0, 0, 0, 0.05),
-					0 25px 50px -12px rgba(0, 0, 0, 0.25)
-				`),
+				"solid-indigo": "0 0 0 2px var(--indigo-500)",
+
+				// "hero-sm": trimWhitespace(`
+				// 	0 0 0 1px rgba(0, 0, 0, 0.05),
+				// 	0 1px 2px 0 rgba(0, 0, 0, 0.05)
+				// `),
+				// "hero": trimWhitespace(`
+				// 	0 0 0 1px rgba(0, 0, 0, 0.05),
+				// 	0 1px 3px 0 rgba(0, 0, 0, 0.1),
+				// 	0 1px 2px 0 rgba(0, 0, 0, 0.06)
+				// `),
+				// "hero-md": trimWhitespace(`
+				// 	0 0 0 1px rgba(0, 0, 0, 0.05),
+				// 	0 4px 6px -1px rgba(0, 0, 0, 0.1),
+				// 	0 2px 4px -1px rgba(0, 0, 0, 0.06)
+				// `),
+				// "hero-lg": trimWhitespace(`
+				// 	0 0 0 1px rgba(0, 0, 0, 0.05),
+				// 	0 10px 15px -3px rgba(0, 0, 0, 0.1),
+				// 	0 4px 6px -2px rgba(0, 0, 0, 0.05)
+				// `),
+				// "hero-xl": trimWhitespace(`
+				// 	0 0 0 1px rgba(0, 0, 0, 0.05),
+				// 	0 20px 25px -5px rgba(0, 0, 0, 0.1),
+				// 	0 10px 10px -5px rgba(0, 0, 0, 0.04)
+				// `),
+				// "hero-2xl": trimWhitespace(`
+				// 	0 0 0 1px rgba(0, 0, 0, 0.05),
+				// 	0 25px 50px -12px rgba(0, 0, 0, 0.25)
+				// `),
 			},
 			fontFamily: {
 				/* eslint-disable quotes */
