ba7dfe08d2e8d3b9eca52de21494bf652ef929e8
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-14 09:48:37 +0900

    Fixed an old link

--- src/App/ExtLinksFragment.js
+++ src/App/ExtLinksFragment.js
@@ -17,7 +17,7 @@ const ExtLinksFragment = () => (
 		</a>
 
 		{/* Star */}
-		<a className="block" href="https://github.com/codex-src/heroicons.dev" {...attrs.target_blank}>
+		<a className="block" href="https://github.com/codex-zaydek/heroicons.dev" {...attrs.target_blank}>
 			<p className="flex flex-row items-center font-medium text-gray-100">
 				{/* <svg className="mr-2 w-5 h-5 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"> */}
 				{/* 	<path fillRule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25z" /> */}
