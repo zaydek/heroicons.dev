59c8df43e6911edc343bde39451bd9642df46fbf
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-26 18:49:44 +0900

    Fixed arrows-expand again. There’s something broken about the optimization.

--- src/pages/index.js
+++ src/pages/index.js
@@ -142,7 +142,7 @@ const Page = () => {
 					{/* Subheader */}
 					<h2 className="flex flex-row justify-center items-center flex-wrap text-center font-medium text-xl sm:text-2xl text-gray-100">
 						<BookOpenSVG className="mr-2 hidden xs:block w-8 h-8" />
-							MIT Open Source Icons by{" "}
+							MIT open source icons by{" "}
 						<span style={{ width: "0.25em" }} />
 						<a
 							className="inline-flex flex-row items-center underline"
@@ -163,7 +163,7 @@ const Page = () => {
 							HEROICONS.DEV IS SPONSORED BY
 					</h6>
 
-					<div className="h-6" />
+					<div className="h-8" />
 					<div className="-my-1 flex flex-row justify-center items-center flex-wrap w-full max-w-3xl">
 
 						<a className="mx-3 my-1 block" href="https://github.com/qawolf/qawolf" {...target_blank}>
@@ -262,51 +262,50 @@ const Page = () => {
 
 					</div>
 
-					<div className="h-6" />
+					<div className="h-8" />
 					<h6 className="text-center font-bold text-sm tracking-wider leading-none text-gray-400">
-							UNAFFILIATED WITH TAILWIND LABS
+						UNAFFILIATED WITH TAILWIND LABS
 					</h6>
 
 					{/* CTA */}
-					<div className="space-x-3 pt-0 sm:pt-12 hidden sm:flex sm:flex-row">
-						<div className="rounded-75 shadow-lg">
-							<a className="px-4 py-3 flex flex-row items-center bg-gray-800 rounded-75 focus:outline-none shadow-none focus:shadow-solid-indigo transition duration-200 ease-in-out" style={{ height: tw(18) }} href="https://figma.com/file/vfjBXrSSOCgmVEX5fdvV4L" {...target_blank}>
-								<div className="px-2 py-1 flex flex-row justify-center">
-									{/* NOTE: Uses text-gray-200 because text-gray-100 is too sharp. */}
-									<p className="flex flex-row items-center font-medium text-xl text-gray-200">
-										<Apply className="mr-4 w-6 h-6">
-											<svg viewBox="0 0 38 57">
-												<path fill="#1abcfe" d="M19 28.5a9.5 9.5 0 1 1 19 0 9.5 9.5 0 0 1-19 0z" />
-												<path fill="#0acf83" d="M0 47.5A9.5 9.5 0 0 1 9.5 38H19v9.5a9.5 9.5 0 1 1-19 0z" />
-												<path fill="#ff7262" d="M19 0v19h9.5a9.5 9.5 0 1 0 0-19H19z" />
-												<path fill="#f24e1e" d="M0 9.5A9.5 9.5 0 0 0 9.5 19H19V0H9.5A9.5 9.5 0 0 0 0 9.5z" />
-												<path fill="#a259ff" d="M0 28.5A9.5 9.5 0 0 0 9.5 38H19V19H9.5A9.5 9.5 0 0 0 0 28.5z" />
-											</svg>
-										</Apply>
-											Open in Figma
-										<ExternalLinkSVG className="ml-2 w-5 h-5 text-indigo-400" />
-									</p>
-								</div>
-							</a>
-						</div>
-						<div className="rounded-75 shadow-lg">
-							<a className="px-4 py-3 flex flex-row items-center bg-gray-800 rounded-75 focus:outline-none shadow-none focus:shadow-solid-indigo transition duration-200 ease-in-out" style={{ height: tw(18) }} href="https://github.com/tailwindlabs/heroicons" {...target_blank}>
-								<div className="px-2 py-1 flex flex-row justify-center">
-									{/* NOTE: Uses text-gray-200 because text-gray-100 is too sharp. */}
-									<p className="flex flex-row items-center font-medium text-xl text-gray-200">
-										{/* <GitHubSVG className="mr-4 w-6 h-6" /> */}
-										<Apply className="mr-4 w-6 h-6">
-											<svg viewBox="0 0 16 16" fill="currentColor">
-												<path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
-											</svg>
-										</Apply>
-											Open in GitHub
-										<ExternalLinkSVG className="ml-2 w-5 h-5 text-indigo-400" />
-									</p>
-								</div>
-							</a>
-						</div>
-					</div>
+					{/* <div className="space-x-3 pt-0 sm:pt-12 hidden sm:flex sm:flex-row"> */}
+					{/* 	<div className="rounded-75 shadow-lg"> */}
+					{/* 		<a className="px-4 py-3 flex flex-row items-center bg-gray-800 rounded-75 focus:outline-none shadow-none focus:shadow-solid-indigo transition duration-200 ease-in-out" style={{ height: tw(18) }} href="https://figma.com/file/vfjBXrSSOCgmVEX5fdvV4L" {...target_blank}> */}
+					{/* 			<div className="px-2 py-1 flex flex-row justify-center"> */}
+					{/* 				// NOTE: Uses text-gray-200 because text-gray-100 is too sharp. */}
+					{/* 				<p className="flex flex-row items-center font-medium text-xl text-gray-200"> */}
+					{/* 					<Apply className="mr-4 w-6 h-6"> */}
+					{/* 						<svg viewBox="0 0 38 57"> */}
+					{/* 							<path fill="#1abcfe" d="M19 28.5a9.5 9.5 0 1 1 19 0 9.5 9.5 0 0 1-19 0z" /> */}
+					{/* 							<path fill="#0acf83" d="M0 47.5A9.5 9.5 0 0 1 9.5 38H19v9.5a9.5 9.5 0 1 1-19 0z" /> */}
+					{/* 							<path fill="#ff7262" d="M19 0v19h9.5a9.5 9.5 0 1 0 0-19H19z" /> */}
+					{/* 							<path fill="#f24e1e" d="M0 9.5A9.5 9.5 0 0 0 9.5 19H19V0H9.5A9.5 9.5 0 0 0 0 9.5z" /> */}
+					{/* 							<path fill="#a259ff" d="M0 28.5A9.5 9.5 0 0 0 9.5 38H19V19H9.5A9.5 9.5 0 0 0 0 28.5z" /> */}
+					{/* 						</svg> */}
+					{/* 					</Apply> */}
+					{/* 						Open in Figma */}
+					{/* 					<ExternalLinkSVG className="ml-2 w-5 h-5 text-indigo-400" /> */}
+					{/* 				</p> */}
+					{/* 			</div> */}
+					{/* 		</a> */}
+					{/* 	</div> */}
+					{/* 	<div className="rounded-75 shadow-lg"> */}
+					{/* 		<a className="px-4 py-3 flex flex-row items-center bg-gray-800 rounded-75 focus:outline-none shadow-none focus:shadow-solid-indigo transition duration-200 ease-in-out" style={{ height: tw(18) }} href="https://github.com/tailwindlabs/heroicons" {...target_blank}> */}
+					{/* 			<div className="px-2 py-1 flex flex-row justify-center"> */}
+					{/* 				// NOTE: Uses text-gray-200 because text-gray-100 is too sharp. */}
+					{/* 				<p className="flex flex-row items-center font-medium text-xl text-gray-200"> */}
+					{/* 					<Apply className="mr-4 w-6 h-6"> */}
+					{/* 						<svg viewBox="0 0 16 16" fill="currentColor"> */}
+					{/* 							<path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" /> */}
+					{/* 						</svg> */}
+					{/* 					</Apply> */}
+					{/* 						Open in GitHub */}
+					{/* 					<ExternalLinkSVG className="ml-2 w-5 h-5 text-indigo-400" /> */}
+					{/* 				</p> */}
+					{/* 			</div> */}
+					{/* 		</a> */}
+					{/* 	</div> */}
+					{/* </div> */}
 
 				</header>
 
