da8a93a8658c20d21bed5325223ed95d07d1fb96
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-27 05:23:04 +0900

    More work on the footer

--- src/pages/_Footer.js
+++ src/pages/_Footer.js
@@ -67,18 +67,23 @@ import FlagSVG from "heroicons-0.4.0/solid/Flag"
 
 const Footer = () => (
 	<footer className="py-12 flex flex-row justify-center border-t-8 border-gray-900">
-		<div className="px-8 flex flex-row justify-between items-center w-full max-w-screen-xl">
+		<div className="px-8 flex flex-row justify-between w-full max-w-screen-xl">
 
 			{/* LHS */}
 			<div>
 
+				<h6 className="font-semibold text-xs tracking-wider text-gray-500">
+					ABOUT
+				</h6>
+
+				<div className="h-4" />
 				<Reset className="block">
 					<a href="TODO" {...target_blank}>
 						<h6
 							className="text-gray-200"
 							style={{
 								fontWeight: 700,
-								fontSize: tw(8),
+								fontSize: tw(10),
 								lineHeight: 1,
 								fontFamily: "'DM Sans', var(--sans)",
 								letterSpacing: "-0.025em",
@@ -86,7 +91,7 @@ const Footer = () => (
 							<span className="inline-flex flex-row items-center">
 								Heroicons{" "}
 								<span style={{ width: "0.25ch" }} />
-								<Apply className="w-8 h-8 text-indigo-400">
+								<Apply className="w-10 h-10 text-indigo-400">
 									<FlagSVG />
 								</Apply>
 							</span>
@@ -95,31 +100,127 @@ const Footer = () => (
 				</Reset>
 
 				<div className="h-4" />
-				<p className="text-gray-200">
-					Icons are designed by Steve Schoger
+
+				<Reset className="block">
+					<a href="TODO" {...target_blank}>
+						<p className="text-gray-200 leading-relaxed" style={{ fontSize: tw(4.75) }}>
+							<span className="inline-flex flex-row items-center">
+								<Apply className="-m-1 mr-0 w-5 h-5">
+									<svg stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
+									</svg>
+									{/* <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"> */}
+									{/* 	<path fillRule="evenodd" d="M17.707 9.293a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-7-7A.997.997 0 012 10V5a3 3 0 013-3h5c.256 0 .512.098.707.293l7 7zM5 6a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" /> */}
+									{/* </svg> */}
+								</Apply>{" "}
+								<span style={{ width: "0.5ch" }} />
+								Version 0.4.0
+							</span>
+						</p>
+					</a>
+				</Reset>
+
+				<Reset className="block">
+					<a href="TODO" {...target_blank}>
+						<p className="text-gray-200 leading-relaxed" style={{ fontSize: tw(4.75) }}>
+							<span className="inline-flex flex-row items-center">
+								<Apply className="-m-1 mr-0 w-5 h-5">
+									<svg stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+									</svg>
+								</Apply>{" "}
+								<span style={{ width: "0.5ch" }} />
+								Heroicons are MIT-licensed
+							</span>
+						</p>
+					</a>
+				</Reset>
+
+				<div className="h-8" />
+				<p className="leading-relaxed text-gray-200">
+					Heroicons are designed by Steve Schoger{" "}
+					<a className="font-medium no-underline hover:underline" href="TODO" {...target_blank}>
+						@steveschoger
+					</a>
 				</p>
 
-				<p className="text-gray-200">
-					Website is designed and maintained by Zaydek MG
+				<p className="leading-relaxed text-gray-200">
+					Website is designed and maintained by Zaydek MG{" "}
+					<a className="font-medium no-underline hover:underline" href="TODO" {...target_blank}>
+						@username_ZAYDEK
+					</a>
+				</p>
+
+				<p className="leading-relaxed text-gray-200">
+					You can contribute on GitHub{" "}
+					<a className="font-medium no-underline hover:underline" href="TODO" {...target_blank}>
+						heroicons.dev
+					</a>
 				</p>
 
 			</div>
 
-			<Reset className="block">
-				<a href="TODO" {...target_blank}>
-					<p className="text-gray-200">
-						<span className="inline-flex flex-row items-center">
-							Share on Twitter{" "}
-							<span style={{ width: "1ch" }} />
-							<Apply className="w-auto h-6 transform scale-110" style={{ color: "#1da1f2" }}>
-								<svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-									<path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z" />
-								</svg>
-							</Apply>
-						</span>
-					</p>
-				</a>
-			</Reset>
+			{/* RHS */}
+			<div>
+
+				<Apply className="text-right">
+					<h6 className="font-semibold text-xs tracking-wider text-gray-500">
+						USEFUL LINKS
+					</h6>
+				</Apply>
+
+				<div className="h-4" />
+				<Reset className="block">
+					<a href="TODO" {...target_blank}>
+						<p className="font-medium text-gray-200">
+							<span className="inline-flex flex-row items-center">
+								Share on Twitter{" "}
+								<span style={{ width: "1ch" }} />
+								<Apply className="w-auto h-6 transform scale-110" style={{ color: "#1da1f2" }}>
+									<svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+										<path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z" />
+									</svg>
+								</Apply>
+							</span>
+						</p>
+					</a>
+				</Reset>
+
+				<div className="h-8" />
+				<Reset className="block">
+					<a href="TODO" {...target_blank}>
+						<p className="font-medium text-gray-200">
+							<span className="inline-flex flex-row items-center">
+								Share on Twitter{" "}
+								<span style={{ width: "1ch" }} />
+								<Apply className="w-auto h-6 transform scale-110" style={{ color: "#1da1f2" }}>
+									<svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+										<path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z" />
+									</svg>
+								</Apply>
+							</span>
+						</p>
+					</a>
+				</Reset>
+
+				<div className="h-4" />
+				<Reset className="block">
+					<a href="TODO" {...target_blank}>
+						<p className="font-medium text-gray-200">
+							<span className="inline-flex flex-row items-center">
+								Share on Twitter{" "}
+								<span style={{ width: "1ch" }} />
+								<Apply className="w-auto h-6 transform scale-110" style={{ color: "#1da1f2" }}>
+									<svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+										<path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z" />
+									</svg>
+								</Apply>
+							</span>
+						</p>
+					</a>
+				</Reset>
+
+			</div>
 
 		</div>
 	</footer>
