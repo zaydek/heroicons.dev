db4b4651f384d570885247b9ce215c2f1a0e566c
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-26 17:35:13 +0900

    Clarifying comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -667,43 +667,26 @@ const MemoIcon = React.memo(({ state, dispatch, icon }) => {
 		}
 
 		try {
-
-			///<svg
-			///	viewBox="0 0 20 20"
-			///	fill="currentColor"
-			///	class="academic-cap w-6 h-6"
-			///	xmlns="http://www.w3.org/2000/svg"
-			///><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
-			///
-			///<svg
-			///	xmlns="http://www.w3.org/2000/svg"
-			///	fill="none"
-			///	viewBox="0 0 24 24"
-			///	stroke="currentColor"
-			///>
-			///  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
-			///</svg>
-
-			// http://www.w3.org/2000/svg
-
 			const originalEl = document.getElementById(icon.name)
 			const el = originalEl.cloneNode(true)
-			el.removeAttribute("id")
 
+			el.removeAttribute("id")
 			el.classList.remove(...el.classList)
 			el.classList.add(...`${icon.name} w-6 h-6`.split(" "))
 			el.setAttribute("xmlns", "http://www.w3.org/2000/svg")
 
+			// Remove unsorted attributes:
 			const sortedAttrs = [...el.attributes].sort((a, b) => a.name.localeCompare(b.name))
 			for (const each of sortedAttrs) {
 				el.removeAttributeNode(each)
 			}
+			// Add sorted attributes:
 			for (const each of sortedAttrs) {
 				el.setAttributeNode(each)
 			}
 
 			copyToClipboardPolyfill(!state.form.copyAsReact ? el.outerHTML : svgToJSX(el.outerHTML))
-			buttonRef.current.focus() // Refocus
+			buttonRef.current.focus()
 		} catch (error) {
 			console.error(`copyToClipboardPolyfill: ${error}`)
 		}
