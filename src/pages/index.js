2b58a50458c060584e49d5ee22aa67f614103372
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-30 01:44:04 +0900

    Added hrefs to CTA buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -147,50 +147,46 @@ const Header = ({ state, dispatch }) => (
 
 				{/* CTA buttons */}
 				<div className="h-6" />
-				<div className="space-x-3">
+				<div className="space-x-3 flex flex-row">
 
-					{false && (
-						<svg fill="currentColor" viewBox="0 0 38 57" xmlns="http://www.w3.org/2000/svg">
-							<path /* fill="#1abcfe" */ d="M19 28.5a9.5 9.5 0 1 1 19 0 9.5 9.5 0 0 1-19 0z" />
-							<path /* fill="#0acf83" */ d="M0 47.5A9.5 9.5 0 0 1 9.5 38H19v9.5a9.5 9.5 0 1 1-19 0z" />
-							<path /* fill="#ff7262" */ d="M19 0v19h9.5a9.5 9.5 0 1 0 0-19H19z" />
-							<path /* fill="#f24e1e" */ d="M0 9.5A9.5 9.5 0 0 0 9.5 19H19V0H9.5A9.5 9.5 0 0 0 0 9.5z" />
-							<path /* fill="#a259ff" */ d="M0 28.5A9.5 9.5 0 0 0 9.5 38H19V19H9.5A9.5 9.5 0 0 0 0 28.5z" />
-						</svg>
-					)}
-
-					{/* Figma */}
-					<button className="px-4 py-3 bg-indigo-50 rounded-3" style={{ "--bg-opacity": 0.1875 }}>
-						<div className="p-1">
-							<p className="font-medium text-lg leading-none text-indigo-50">
-								<span className="inline-flex flex-row items-center">
-									<Apply className="mr-1 w-5 h-5">
-										<DownloadSVG />
-									</Apply>{" "}
-									<span style={{ width: "0.5ch" }} />
-									Download All
-								</span>
-							</p>
+					{/* Download */}
+					{/* */}
+					{/* TODO: Implement download. */}
+					<a href="https://github.com/tailwindlabs/heroicons/archive/v0.4.1.zip">
+						<div className="px-4 py-3 bg-indigo-50 rounded-3" style={{ "--bg-opacity": 0.1875 }}>
+							<div className="p-1">
+								<p className="font-medium text-lg leading-none text-indigo-50">
+									<span className="inline-flex flex-row items-center">
+										<Apply className="mr-1 w-5 h-5">
+											<DownloadSVG />
+										</Apply>{" "}
+										<span style={{ width: "0.5ch" }} />
+										Download All
+									</span>
+								</p>
+							</div>
 						</div>
-					</button>
+					</a>
 
 					{/* GitHub */}
-					<button className="px-4 py-3 bg-indigo-50 rounded-3" style={{ "--bg-opacity": 0.1875 }}>
-						<div className="p-1">
-							<p className="font-medium text-lg leading-none text-indigo-50">
-								<span className="inline-flex flex-row items-center">
-									<Apply className="mr-1 w-5 h-5">
-										<svg fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-											<path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
-										</svg>
-										{/* <ExternalLinkOutlineSVG /> */}
-									</Apply>{" "}
-									<span style={{ width: "0.5ch" }} />
-									Open in GitHub
-								</span>
-							</p>
+					<a href="https://github.com/tailwindlabs/heroicons">
+						<div className="px-4 py-3 bg-indigo-50 rounded-3" style={{ "--bg-opacity": 0.1875 }}>
+							<div className="p-1">
+								<p className="font-medium text-lg leading-none text-indigo-50">
+									<span className="inline-flex flex-row items-center">
+										<Apply className="mr-1 w-5 h-5">
+											<svg fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+												<path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
+											</svg>
+											{/* <ExternalLinkOutlineSVG /> */}
+										</Apply>{" "}
+										<span style={{ width: "0.5ch" }} />
+										Open in GitHub
+									</span>
+								</p>
+							</div>
 						</div>
-					</button>
+					</a>
 
 				</div>
 
@@ -663,11 +659,12 @@ html {
 			<section>
 				<Header state={state} dispatch={dispatch} />
 			</section>
-			<section className="-mt-48 px-8 flex flex-row justify-center">
+			<section className="-mt-48 px-6 flex flex-row justify-center">
 				<div className="w-full z-10" style={{ maxWidth: 1440 }}>
 					<Main state={state} dispatch={dispatch} />
 				</div>
 			</section>
+			{/* <div className="h-24" /> */}
 		</>
 	)
 }
