c007ed0cc19c1a91294e9e7bce89b2d9f2da2881
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-09-01 07:12:47 +0900

    Prototyped the LHS search bar icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,16 @@
 import Apply from "lib/x/Apply"
+import disableAutoCorrect from "lib/x/disableAutoCorrect"
 import px from "lib/x/pxToRem"
 import Reset from "lib/x/Reset"
 import tw from "lib/x/twToRem"
+
+import SVGSearchOutline from "heroicons-0.4.1/outline/Search"
 
 const Hero = () => (
 	<div className="!bg-gray-300" style={{ height: px(30 * 16 /* 32 * 16 */) }}>
 		{/* ... */}
 	</div>
 )
-
-// const SearchBar = () => (
-// 	<div className="rounded-6 shadow-2">
-// 		<div className="relative rounded-6" style={{ height: px(72), boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }}>
-//
-// 			{/* LHS */}
-// 			<div className="absolute left-0 inset-y-0">
-// 				lhs
-// 			</div>
-//
-// 			{/* Search */}
-// 			<Reset className="block w-full h-full">
-// 				<input
-// 					className="rounded-6"
-// 					// style={{ borderRadius: "2rem" }}
-// 				/>
-// 			</Reset>
-//
-// 			{/* RHS */}
-// 			<div className="absolute right-0 inset-y-0">
-// 				rhs
-// 			</div>
-//
-// 		</div>
-// 	</div>
-// )
 
 const Search = () => (
 	// NOTE: Uses h-full because of absolute inset-0.
@@ -42,14 +19,22 @@ const Search = () => (
 
 			{/* LHS */}
 			<div className="absolute left-0 inset-y-0">
-				{/* ... */}
+				<div className="px-8 pr-4 flex flex-row items-center h-full">
+					<Apply className="w-6 h-6 text-gray-400">
+						<SVGSearchOutline />
+					</Apply>
+				</div>
 			</div>
 
 			{/* Search */}
 			<Reset className="block w-full h-full focus:outline-none">
 				<input
-					className="px-16 rounded-6"
-					style={{ boxShadow: "0 0 0 4px var(--indigo-500)" }}
+					className="px-16 text-xl text-gray-800 bg-white rounded-6"
+					style={{
+						paddingLeft: tw(8 + 6 + 4),
+						boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)",
+					}}
+					{...disableAutoCorrect}
 				/>
 			</Reset>
 
@@ -84,14 +69,14 @@ const App = () => (
 				{/* Search */}
 				<div className="sticky z-10" style={{ top: tw(4) }}>
 					<div className="rounded-6 shadow-2">
-						<div className="rounded-6" style={{ height: px(72), boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }}>
+						<div className="rounded-6" style={{ height: px(72) }}>
 							<Search />
 						</div>
 					</div>
 				</div>
 
 				{/* Icons */}
-				<div style={{ height: px(16) }} />
+				<div style={{ height: px(24) }} />
 				<div className="rounded-6 shadow-2">
 					{/* TODO: Drop overflow-hidden here? */}
 					<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 bg-white rounded-6 overflow-hidden" style={{ boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }}>
