4b17cd9ef10ed443f45bb8e9fa4d5a5026867f72
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-09-01 07:41:40 +0900

    Prototyped the new label

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,23 @@ import tw from "lib/x/twToRem"
 
 import SVGSearchOutline from "heroicons-0.4.1/outline/Search"
 
+// hsl(272.5, 100%, 47.5%)
+
 const Hero = () => (
-	<div className="bg-gray-300" style={{ height: px(30 * 16 /* 32 * 16 */) }}>
-		{/* ... */}
+	<div className="relative">
+
+		<div className="bg-gray-200" style={{ height: px(32 * 16) }}>
+		</div>
+
+		{/* <div className="fixed inset-x-0 top-0 -z-10"> */}
+		{/* 	<div className="h-24 bg-gray-200" /> */}
+		{/* 	<Apply className="h-auto text-gray-200"> */}
+		{/* 		<svg fill="currentColor" viewBox="0 0 8 1" xmlns="http://www.w3.org/2000/svg"> */}
+		{/* 			<path fillRule="evenodd" clipRule="evenodd" d="M4 0.999999C2 1 0.5 0.333333 0 0L8 -1.39876e-06C7.41667 0.333332 6 0.999999 4 0.999999Z" /> */}
+		{/* 		</svg> */}
+		{/* 	</Apply> */}
+		{/* </div> */}
+
 	</div>
 )
 
@@ -53,6 +67,11 @@ const Icon = () => (
 		<div className="relative" style={{ boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }}>
 
 			{/* New */}
+			{!Math.floor(Math.random() * 5) && (
+				<div className="p-4 absolute right-0 top-0">
+					<div className="w-3 h-3 bg-gray-300 rounded-full animate-pulse" />
+				</div>
+			)}
 
 			{/* Icon */}
 			<div className="flex flex-row justify-center items-center h-full">
@@ -75,22 +94,22 @@ function range(max) {
 }
 
 const App = () => (
-	<Apply style={{ marginTop: px(-1 * (72 + 24)) }}>
+	<Apply style={{ marginTop: tw(-(18 + 6)) }}>
 		{/* NOTE: Uses items-start because of sticky top-*. */}
 		<div className="px-4 flex flex-row justify-center items-start">
-			<div style={{ width: "100%", maxWidth: px(1152) }}>
+			<div className="w-full" style={{ maxWidth: px(1152) }}>
 
 				{/* Search */}
-				<div className="sticky z-10" style={{ top: tw(4) }}>
+				<div className="sticky top-0 z-10" /* style={{ top: tw(4) }} */>
 					<div className="rounded-6 shadow-2">
-						<div className="rounded-6" style={{ height: px(72) }}>
+						<div className="rounded-6" style={{ height: tw(18) }}>
 							<Search />
 						</div>
 					</div>
 				</div>
 
 				{/* Icons */}
-				<div style={{ height: px(24) }} />
+				<div style={{ height: tw(6) }} />
 				<div className="rounded-6 shadow-2">
 					{/* TODO: Drop overflow-hidden here? */}
 					<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 bg-white rounded-6 overflow-hidden" style={{ boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }}>
@@ -125,6 +144,10 @@ const Layout = () => (
 		<main>
 			<App />
 		</main>
+
+		{/* TODO: Remove. */}
+		<div className="h-24" />
+
 	</div>
 )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,13 @@ module.exports = {
 			fontFamily: {
 				sans: [...defaultTheme.fontFamily.sans.slice(0, 3), "Inter", ...defaultTheme.fontFamily.sans.slice(3)],
 			},
+			zIndex: {
+				"-50": "-50",
+				"-40": "-40",
+				"-30": "-30",
+				"-20": "-20",
+				"-10": "-10",
+			},
 		},
 		screens: {
 			/* eslint-disable no-multi-spaces */
