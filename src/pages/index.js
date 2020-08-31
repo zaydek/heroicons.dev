4bdbecc2662c0d2c8204f52a318b72f68914ae3e
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-09-01 08:46:59 +0900

    Tweaks to CTA buttons implementation

--- src/pages/index.js
+++ src/pages/index.js
@@ -262,44 +262,40 @@ const Attribution = () => (
 )
 
 
-{/* CTA buttons */}
 const CTAButtons = () => (
-	<div className="space-x-3 flex flex-row">
+	<div className="flex flex-col sm:flex-row w-full sm:w-auto">
 
 		<Reset className="block">
 			<a href="https://github.com/tailwindlabs/heroicons/archive/v0.4.1.zip">
-				<div className="px-4 py-3 bg-gray-100 rounded-3">
-					<div className="p-1">
-						<p className="whitespace-pre font-medium text-lg leading-none text-gray-800">
-							<span className="inline-flex flex-row items-center">
-								<Apply className="mr-1 w-5 h-5">
-									<SVGDownload />
-								</Apply>
-								<span style={{ width: "0.5ch" }} />
-								Download All
-							</span>
-						</p>
-					</div>
+				<div className="px-5 py-4 bg-gray-100 rounded-3">
+					<p className="whitespace-pre text-center sm:text-left font-medium text-lg leading-none text-gray-800">
+						<span className="inline-flex flex-row items-center">
+							<Apply className="mr-1 w-5 h-5">
+								<SVGDownload />
+							</Apply>
+							<span style={{ width: "0.5ch" }} />
+							Download Icons
+						</span>
+					</p>
 				</div>
 			</a>
 		</Reset>
 
+		<div className="w-3 h-3" />
 		<Reset className="block">
 			<a href="https://github.com/tailwindlabs/heroicons" {...target_blank}>
-				<div className="px-4 py-3 bg-gray-100 rounded-3">
-					<div className="p-1">
-						<p className="whitespace-pre font-medium text-lg leading-none text-gray-800">
-							<span className="inline-flex flex-row items-center">
-								<Apply className="mr-1 w-5 h-5">
-									<svg fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-										<path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
-									</svg>
-								</Apply>
-								<span style={{ width: "0.5ch" }} />
-								Open in GitHub
-							</span>
-						</p>
-					</div>
+				<div className="px-5 py-4 bg-gray-100 rounded-3">
+					<p className="whitespace-pre text-center sm:text-left font-medium text-lg leading-none text-gray-800">
+						<span className="inline-flex flex-row items-center">
+							<Apply className="mr-1 w-5 h-5">
+								<svg fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+									<path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
+								</svg>
+							</Apply>
+							<span style={{ width: "0.5ch" }} />
+							Open in GitHub
+						</span>
+					</p>
 				</div>
 			</a>
 		</Reset>
