1b23e3ca8b4b08171425bd9b6c35326310b70146
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-09-01 06:58:16 +0900

    Prototyped the bare-bones search bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,74 @@
 import Apply from "lib/x/Apply"
 import px from "lib/x/pxToRem"
-// import tw from "lib/x/twToRem"
+import Reset from "lib/x/Reset"
+import tw from "lib/x/twToRem"
 
 const Hero = () => (
-	<header className="!bg-gray-300" style={{ height: px(30 * 16 /* 32 * 16 */) }}>
+	<div className="!bg-gray-300" style={{ height: px(30 * 16 /* 32 * 16 */) }}>
 		{/* ... */}
-	</header>
+	</div>
+)
+
+// const SearchBar = () => (
+// 	<div className="rounded-6 shadow-2">
+// 		<div className="relative rounded-6" style={{ height: px(72), boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }}>
+//
+// 			{/* LHS */}
+// 			<div className="absolute left-0 inset-y-0">
+// 				lhs
+// 			</div>
+//
+// 			{/* Search */}
+// 			<Reset className="block w-full h-full">
+// 				<input
+// 					className="rounded-6"
+// 					// style={{ borderRadius: "2rem" }}
+// 				/>
+// 			</Reset>
+//
+// 			{/* RHS */}
+// 			<div className="absolute right-0 inset-y-0">
+// 				rhs
+// 			</div>
+//
+// 		</div>
+// 	</div>
+// )
+
+const Search = () => (
+	// NOTE: Uses h-full because of absolute inset-0.
+	<Apply className="h-full">
+		<div className="relative">
+
+			{/* LHS */}
+			<div className="absolute left-0 inset-y-0">
+				{/* ... */}
+			</div>
+
+			{/* Search */}
+			<Reset className="block w-full h-full focus:outline-none">
+				<input
+					className="px-16 rounded-6"
+					style={{ boxShadow: "0 0 0 4px var(--indigo-500)" }}
+				/>
+			</Reset>
+
+			{/* RHS */}
+			<div className="absolute right-0 inset-y-0">
+				{/* ... */}
+			</div>
+
+		</div>
+	</Apply>
 )
 
 const Icon = () => (
-	<div className="bg-red-200 shadow-2">
-		hello
-	</div>
+	// NOTE: Uses h-full because of absolute inset-0.
+	<Apply className="h-full">
+		<div style={{ boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }}>
+			{/* */}
+		</div>
+	</Apply>
 )
 
 function range(max) {
@@ -20,48 +77,55 @@ function range(max) {
 
 const App = () => (
 	<Apply className="-mt-72" style={{ marginTop: px(-1 * (72 + 16)) }}>
-		<main className="flex flex-row justify-center">
+		{/* NOTE: Uses items-start because of sticky top-*. */}
+		<div className="flex flex-row justify-center items-start">
 			<div style={{ width: "100%", maxWidth: px(1152) }}>
 
 				{/* Search */}
-				<div className="rounded-6 shadow-2">
-					<div className="bg-white rounded-6" style={{ height: px(72), boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }}>
-
+				<div className="sticky z-10" style={{ top: tw(4) }}>
+					<div className="rounded-6 shadow-2">
+						<div className="rounded-6" style={{ height: px(72), boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }}>
+							<Search />
+						</div>
 					</div>
 				</div>
 
 				{/* Icons */}
 				<div style={{ height: px(16) }} />
 				<div className="rounded-6 shadow-2">
-					<main className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 bg-white rounded-6 overflow-hidden" style={{ boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }}>
+					{/* TODO: Drop overflow-hidden here? */}
+					<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 bg-white rounded-6 overflow-hidden" style={{ boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }}>
 
 						{range(200).map((each, x) => (
 							// TODO: each.name
 							<div key={each.name} className="pb-full relative">
 								<div className="absolute inset-0">
-									<div className="h-full red-100" style={{ boxShadow: "inset 0 0 0 0.5px var(--gray-300), 0 0 0 0.5px var(--gray-300)" }} />
 									{/* <MemoIcon */}
 									{/* 	state={state} */}
 									{/* 	dispatch={dispatch} */}
 									{/* 	icon={each} */}
 									{/* /> */}
-									{/* <Icon className="h-full" /> */}
+									<Icon />
 								</div>
 							</div>
 						))}
 
-					</main>
+					</div>
 				</div>
 
 			</div>
-		</main>
+		</div>
 	</Apply>
 )
 
 const Layout = () => (
 	<div id="next-root">
-		<Hero />
-		<App />
+		<header>
+			<Hero />
+		</header>
+		<main>
+			<App />
+		</main>
 	</div>
 )
 
