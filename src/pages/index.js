eff01b2c38b47f6bd750d1afc6b28d9577276914
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-09-02 08:10:12 +0900

    Fixed the broken JSX transition (components break the CSS transition in this class because components remount entirely)

--- src/pages/index.js
+++ src/pages/index.js
@@ -411,11 +411,25 @@ const Search = () => {
 									}}
 								>
 									<Apply className="transition duration-200 ease-in-out">
-										{!copyAsJSX ? (
-											<SVGCodeOutline />
-										) : (
-											<SVGCode />
-										)}
+										{/* NOTE: Use SVGs not React components
+										because React remounts components. *}
+										{/* {!copyAsJSX ? ( */}
+										{/* 	<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"> */}
+										{/* 		<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" /> */}
+										{/* 	</svg> */}
+										{/* ) : ( */}
+										{/* 	<svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"> */}
+										{/* 		<path fillRule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" /> */}
+										{/* 	</svg> */}
+										{/* )} */}
+										<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth={!copyAsJSX ? 2 : 2.4}
+												d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+											/>
+										</svg>
 									</Apply>
 								</Apply>
 							</button>
