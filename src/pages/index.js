a853fb8b48fe223410bac0e3ef304737bdacde79
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-09-01 10:33:47 +0900

    0.25ch and 0.5ch -> 0.33ch

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,14 +45,14 @@ const Attribution = () => (
 			<Apply className="w-5 h-5">
 				<SVGBookOpen />
 			</Apply>
-			<span style={{ width: "0.25ch" }} />
+			<span style={{ width: "0.33ch" }} />
 			MIT icons by
-			<span style={{ width: "0.25ch" }} />
+			<span style={{ width: "0.33ch" }} />
 			<span className="font-semibold no-underline hover:underline">
 				@steveschoger
 			</span>
 			. Third-party viewer by
-			<span style={{ width: "0.25ch" }} />
+			<span style={{ width: "0.33ch" }} />
 			<span className="font-semibold no-underline hover:underline">
 				@username_ZAYDEK
 			</span>
@@ -80,7 +80,7 @@ const CTAButtons = () => (
 									<path fill="#a259ff" d="M0 28.5A9.5 9.5 0 0 0 9.5 38H19V19H9.5A9.5 9.5 0 0 0 0 28.5z" />
 								</svg>
 							</Apply>
-							<span style={{ width: "0.5ch" }} />
+							<span style={{ width: "0.33ch" }} />
 							Open in Figma
 						</span>
 					</p>
@@ -99,7 +99,7 @@ const CTAButtons = () => (
 									<path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
 								</svg>
 							</Apply>
-							<span style={{ width: "0.5ch" }} />
+							<span style={{ width: "0.33ch" }} />
 							Open in GitHub
 						</span>
 					</p>
@@ -120,7 +120,7 @@ const Sponsors = () => (
 				<h6 className="text-center font-bold tracking-wider leading-none text-gray-800" style={{ fontSize: "0.6875rem" }}>
 					<span className="inline-flex flex-row items-center">
 						OUR SPONSORS &middot; UNAFFILIATED WITH TAILWIND LABS
-						<span style={{ width: "0.5ch" }} />
+						<span style={{ width: "0.33ch" }} />
 						<Apply className="-mb-px w-3 h-3">
 							<SVGBeaker />
 						</Apply>
@@ -135,7 +135,7 @@ const Sponsors = () => (
 				<h6 className="font-bold tracking-wider leading-none text-gray-800" style={{ fontSize: "0.6875rem" }}>
 					<span className="inline-flex flex-row items-center">
 						OUR SPONSORS &middot; BECOME A SPONSOR
-						<span style={{ width: "0.5ch" }} />
+						<span style={{ width: "0.33ch" }} />
 						<Apply className="-mb-px w-3 h-3">
 							<SVGExternalLink />
 						</Apply>
@@ -150,7 +150,7 @@ const Sponsors = () => (
 				<h6 className="font-bold tracking-wider leading-none text-gray-800" style={{ fontSize: "0.6875rem" }}>
 					<span className="inline-flex flex-row items-center">
 						UNAFFILIATED WITH TAILWIND LABS
-						<span style={{ width: "0.5ch" }} />
+						<span style={{ width: "0.33ch" }} />
 						<Apply className="-mb-px w-3 h-3">
 							<SVGBeaker />
 						</Apply>
@@ -176,7 +176,7 @@ const Sponsors = () => (
 							<span className="opacity-75">
 								&rdquo;
 							</span>
-							<span style={{ width: "0.5ch" }} />
+							<span style={{ width: "0.33ch" }} />
 							<Apply className="w-3 h-3 opacity-75">
 								<SVGExternalLink />
 							</Apply>
