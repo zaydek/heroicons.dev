754550f0b12f7bd248c5dff48a09edbf5a35c529
Zaydek <zaydekdotcom@gmail.com>
2023-01-05 18:43:06 -0800

    UnoCSS tweaks

--- unocss.config.ts
+++ unocss.config.ts
@@ -3,8 +3,6 @@ import transformerVariantGroup from "@unocss/transformer-variant-group"
 import { Rule } from "unocss"
 import { defineConfig } from "unocss/vite"
 
-//// import { cssSpec } from "./unocss.css.spec"
-
 function convertSpaces(value: string) {
 	return value.replace(/_/g, " ")
 }
@@ -19,7 +17,7 @@ function desugar(rawValue: undefined | string, { sign, px }: { sign?: string, px
 
 	if (rawValue === undefined) { return undefined }
 
-	const value = extractCSSVariables(convertSpaces(rawValue))
+	const value = convertSpaces(extractCSSVariables(rawValue))
 	if (value.startsWith("$")) {
 		return value
 	} else if (value.startsWith("[") && value.endsWith("]")) {
@@ -62,113 +60,105 @@ const rules: Rule[] = [
 	["relative", { "position": "relative" }],
 	["sticky",   { "position": "sticky"   }],
 
-	[/^(-?)inset-(.+)$/,         ([_, sign, value]) => ({ "inset":   desugar(value, { sign }) })],
-	[/^(-?)inset-x-(.+)$/,       ([_, sign, value]) => ({ "top":     "auto", "right": desugar(value, { sign }), "bottom": "auto", "left": desugar(value, { sign }) })],
-	[/^(-?)inset-y-(.+)$/,       ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": "auto", "bottom": desugar(value, { sign }), "left": "auto" })],
-	[/^(-?)inset-t-(.+)$/,       ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": desugar(value, { sign }), "bottom": "auto", "left": desugar(value, { sign }) })],
-	[/^(-?)inset-r-(.+)$/,       ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": desugar(value, { sign }), "bottom": desugar(value, { sign }), "left": "auto" })],
-	[/^(-?)inset-b-(.+)$/,       ([_, sign, value]) => ({ "top":     "auto", "right": desugar(value, { sign }), "bottom": desugar(value, { sign }), "left": desugar(value, { sign }) })],
-	[/^(-?)inset-l-(.+)$/,       ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": "auto", "bottom": desugar(value, { sign }), "left": desugar(value, { sign }) })],
-	[/^(-?)inset-tr-(.+)$/,      ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": desugar(value, { sign }), "bottom": "auto", "left": "auto" })],
-	[/^(-?)inset-br-(.+)$/,      ([_, sign, value]) => ({ "top":     "auto", "right": desugar(value, { sign }), "bottom": desugar(value, { sign }), "left": "auto" })],
-	[/^(-?)inset-bl-(.+)$/,      ([_, sign, value]) => ({ "top":     "auto", "right": "auto", "bottom": desugar(value, { sign }), "left": desugar(value, { sign }) })],
-	[/^(-?)inset-tl-(.+)$/,      ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": "auto", "bottom": "auto", "left": desugar(value, { sign }) })],
-	[/^(-?)t-(.+)$/,             ([_, sign, value]) => ({ "top":     desugar(value, { sign }) })],
-	[/^(-?)r-(.+)$/,             ([_, sign, value]) => ({ "right":   desugar(value, { sign }) })],
-	[/^(-?)b-(.+)$/,             ([_, sign, value]) => ({ "bottom":  desugar(value, { sign }) })],
-	[/^(-?)l-(.+)$/,             ([_, sign, value]) => ({ "left":    desugar(value, { sign }) })],
-	[/^z-(.+)$/,                 ([_, value]) =>       ({ "z-index": desugar(value, { px: false }) })],
+	[/^(-?)inset-(.+)$/,    ([_, sign, value]) => ({ "inset":   desugar(value, { sign }) })],
+	[/^(-?)inset-x-(.+)$/,  ([_, sign, value]) => ({ "top":     "auto", "right": desugar(value, { sign }), "bottom": "auto", "left": desugar(value, { sign }) })],
+	[/^(-?)inset-y-(.+)$/,  ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": "auto", "bottom": desugar(value, { sign }), "left": "auto" })],
+	[/^(-?)inset-t-(.+)$/,  ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": desugar(value, { sign }), "bottom": "auto", "left": desugar(value, { sign }) })],
+	[/^(-?)inset-r-(.+)$/,  ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": desugar(value, { sign }), "bottom": desugar(value, { sign }), "left": "auto" })],
+	[/^(-?)inset-b-(.+)$/,  ([_, sign, value]) => ({ "top":     "auto", "right": desugar(value, { sign }), "bottom": desugar(value, { sign }), "left": desugar(value, { sign }) })],
+	[/^(-?)inset-l-(.+)$/,  ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": "auto", "bottom": desugar(value, { sign }), "left": desugar(value, { sign }) })],
+	[/^(-?)inset-tr-(.+)$/, ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": desugar(value, { sign }), "bottom": "auto", "left": "auto" })],
+	[/^(-?)inset-br-(.+)$/, ([_, sign, value]) => ({ "top":     "auto", "right": desugar(value, { sign }), "bottom": desugar(value, { sign }), "left": "auto" })],
+	[/^(-?)inset-bl-(.+)$/, ([_, sign, value]) => ({ "top":     "auto", "right": "auto", "bottom": desugar(value, { sign }), "left": desugar(value, { sign }) })],
+	[/^(-?)inset-tl-(.+)$/, ([_, sign, value]) => ({ "top":     desugar(value, { sign }), "right": "auto", "bottom": "auto", "left": desugar(value, { sign }) })],
+	[/^(-?)t-(.+)$/,        ([_, sign, value]) => ({ "top":     desugar(value, { sign }) })],
+	[/^(-?)r-(.+)$/,        ([_, sign, value]) => ({ "right":   desugar(value, { sign }) })],
+	[/^(-?)b-(.+)$/,        ([_, sign, value]) => ({ "bottom":  desugar(value, { sign }) })],
+	[/^(-?)l-(.+)$/,        ([_, sign, value]) => ({ "left":    desugar(value, { sign }) })],
+	[/^z-(.+)$/,            ([_, value]) =>       ({ "z-index": desugar(value, { px: false }) })],
 
 	/*
 	 * Spacing
 	 */
-	[/^(-?)m-(.+)$/,             ([_, sign, value]) => ({ "margin":         desugar(value, { sign }) })],
-	[/^(-?)my-(.+)$/,            ([_, sign, value]) => ({ "margin-top":     desugar(value, { sign }), "margin-bottom": desugar(value, { sign }) })],
-	[/^(-?)mx-(.+)$/,            ([_, sign, value]) => ({ "margin-right":   desugar(value, { sign }), "margin-left":   desugar(value, { sign }) })],
-	[/^(-?)mt-(.+)$/,            ([_, sign, value]) => ({ "margin-top":     desugar(value, { sign }) })],
-	[/^(-?)mr-(.+)$/,            ([_, sign, value]) => ({ "margin-right":   desugar(value, { sign }) })],
-	[/^(-?)mb-(.+)$/,            ([_, sign, value]) => ({ "margin-bottom":  desugar(value, { sign }) })],
-	[/^(-?)ml-(.+)$/,            ([_, sign, value]) => ({ "margin-left":    desugar(value, { sign }) })],
-	[/^p-(.+)$/,                 ([_, value])       => ({ "padding":        desugar(value) })],
-	[/^py-(.+)$/,                ([_, value])       => ({ "padding-top":    desugar(value), "padding-bottom": desugar(value) })],
-	[/^px-(.+)$/,                ([_, value])       => ({ "padding-right":  desugar(value), "padding-left":   desugar(value) })],
-	[/^pt-(.+)$/,                ([_, value])       => ({ "padding-top":    desugar(value) })],
-	[/^pr-(.+)$/,                ([_, value])       => ({ "padding-right":  desugar(value) })],
-	[/^pb-(.+)$/,                ([_, value])       => ({ "padding-bottom": desugar(value) })],
-	[/^pl-(.+)$/,                ([_, value])       => ({ "padding-left":   desugar(value) })],
+	[/^(-?)m-(.+)$/,        ([_, sign, value]) => ({ "margin":         desugar(value, { sign }) })],
+	[/^(-?)my-(.+)$/,       ([_, sign, value]) => ({ "margin-top":     desugar(value, { sign }), "margin-bottom": desugar(value, { sign }) })],
+	[/^(-?)mx-(.+)$/,       ([_, sign, value]) => ({ "margin-right":   desugar(value, { sign }), "margin-left":   desugar(value, { sign }) })],
+	[/^(-?)mt-(.+)$/,       ([_, sign, value]) => ({ "margin-top":     desugar(value, { sign }) })],
+	[/^(-?)mr-(.+)$/,       ([_, sign, value]) => ({ "margin-right":   desugar(value, { sign }) })],
+	[/^(-?)mb-(.+)$/,       ([_, sign, value]) => ({ "margin-bottom":  desugar(value, { sign }) })],
+	[/^(-?)ml-(.+)$/,       ([_, sign, value]) => ({ "margin-left":    desugar(value, { sign }) })],
+	[/^p-(.+)$/,            ([_, value])       => ({ "padding":        desugar(value) })],
+	[/^py-(.+)$/,           ([_, value])       => ({ "padding-top":    desugar(value), "padding-bottom": desugar(value) })],
+	[/^px-(.+)$/,           ([_, value])       => ({ "padding-right":  desugar(value), "padding-left":   desugar(value) })],
+	[/^pt-(.+)$/,           ([_, value])       => ({ "padding-top":    desugar(value) })],
+	[/^pr-(.+)$/,           ([_, value])       => ({ "padding-right":  desugar(value) })],
+	[/^pb-(.+)$/,           ([_, value])       => ({ "padding-bottom": desugar(value) })],
+	[/^pl-(.+)$/,           ([_, value])       => ({ "padding-left":   desugar(value) })],
 
 	/*
 	 * Flexbox
 	 */
-	[/^justify-self-(.+)$/,      ([_, value]) => ({ "justify-self": desugar(value, { px: false }) })],
-	[/^flex-align-self-(.+)$/,   ([_, value]) => ({ "align-self":   desugar(value, { px: false }) })],
-	[/^flex-grow-(.+)$/,         ([_, value]) => ({ "flex-grow":    desugar(value, { px: false }) })],
-	[/^flex-shrink-(.+)$/,       ([_, value]) => ({ "flex-shrink":  desugar(value, { px: false }) })],
-	[/^flex-basis-(.+)$/,        ([_, value]) => ({ "flex-basis":   desugar(value) })],
-	//// [/^flex-(.+)$/,              ([_, value]) => ({ "flex":         desugar(value) })],
+	[/^justify-self-(.+)$/, ([_, value]) => ({ "justify-self": desugar(value, { px: false }) })],
+	[/^align-self-(.+)$/,   ([_, value]) => ({ "align-self":   desugar(value, { px: false }) })],
+	[/^flex-grow-(.+)$/,    ([_, value]) => ({ "flex-grow":    desugar(value, { px: false }) })],
+	[/^flex-shrink-(.+)$/,  ([_, value]) => ({ "flex-shrink":  desugar(value, { px: false }) })],
+	[/^flex-basis-(.+)$/,   ([_, value]) => ({ "flex-basis":   desugar(value) })],
 
-	["flex", { "display": "flex" }],
-
+	["flex",     { "display": "flex" }],
 	["flex-row", { "flex-direction": "row" }],
 	["flex-col", { "flex-direction": "column" }],
 
-	[/^justify-(.+)$/,           ([_, value]) => ({ "justify-content": desugar(value, { px: false }) })],
-	[/^align-(.+)$/,             ([_, value]) => ({ "align-items":     desugar(value, { px: false }) })],
-	//// [/^flex-wrap-(.+)$/,         ([_, value]) => ({ "flex-wrap":       desugar(value, { px: false }) })],
-
-	// Shorthand
-	//// [/^flex-(.+)$/,              ([_, value]) => ({ "justify-content": desugar(value), "align-items": desugar(value) })],
+	[/^justify-(.+)$/,      ([_, value]) => ({ "justify-content": desugar(value, { px: false }) })],
+	[/^align-(.+)$/,        ([_, value]) => ({ "align-items":     desugar(value, { px: false }) })],
+	[/^flex-wrap-(.+)$/,    ([_, value]) => ({ "flex-wrap":       desugar(value, { px: false }) })],
 
 	/*
 	 * CSS Grid
 	 */
 	["grid", { "display": "grid" }],
 
-	[/^grid-(\d+)$/,             ([_, x]) => ({ "display": "grid", "grid-template":         x === "1" ? "1fr" : `repeat(${x}, 1fr)` })], // TODO
-	[/^grid-rows-(\d+)$/,        ([_, y]) => ({ "display": "grid", "grid-template-rows":    y === "1" ? "1fr" : `repeat(${y}, 1fr)` })], // TODO
-	[/^grid-cols-(\d+)$/,        ([_, x]) => ({ "display": "grid", "grid-template-columns": x === "1" ? "1fr" : `repeat(${x}, 1fr)` })], // TODO
-
-	// Shorthand
-	//// ["grid-center", { "place-items": "center" }],
+	[/^grid-(\d+)$/,        ([_, value]) => ({ "display": "grid", "grid-template":         value === "1" ? "1fr" : `repeat(${desugar(value, { px: false })}, 1fr)` })],
+	[/^grid-rows-(\d+)$/,   ([_, value]) => ({ "display": "grid", "grid-template-rows":    value === "1" ? "1fr" : `repeat(${desugar(value, { px: false })}, 1fr)` })],
+	[/^grid-cols-(\d+)$/,   ([_, value]) => ({ "display": "grid", "grid-template-columns": value === "1" ? "1fr" : `repeat(${desugar(value, { px: false })}, 1fr)` })],
 
 	/*
 	 * Gap
 	 */
-	[/^gap-(.+)$/,               ([_, value]) => ({ "gap":        desugar(value) })],
-	[/^gap-y-(.+)$/,             ([_, value]) => ({ "row-gap":    desugar(value) })],
-	[/^gap-x-(.+)$/,             ([_, value]) => ({ "column-gap": desugar(value) })],
+	[/^gap-(.+)$/,          ([_, value]) => ({ "gap":        desugar(value) })],
+	[/^gap-y-(.+)$/,        ([_, value]) => ({ "row-gap":    desugar(value) })],
+	[/^gap-x-(.+)$/,        ([_, value]) => ({ "column-gap": desugar(value) })],
 
 	/*
 	 * Sizing
 	 */
-	[/^h-(.+)$/,                 ([_, value]) => ({ "height":       desugar(value) })],
-	[/^min-h-(.+)$/,             ([_, value]) => ({ "min-height":   desugar(value) })],
-	[/^max-h-(.+)$/,             ([_, value]) => ({ "max-height":   desugar(value) })],
-	[/^w-(.+)$/,                 ([_, value]) => ({ "width":        desugar(value) })],
-	[/^min-w-(.+)$/,             ([_, value]) => ({ "min-width":    desugar(value) })],
-	[/^max-w-(.+)$/,             ([_, value]) => ({ "max-width":    desugar(value) })],
-	[/^aspect-(.+)$/,            ([_, value]) => ({ "aspect-ratio": desugar(value, { px: false }) })],
+	[/^h-(.+)$/,            ([_, value]) => ({ "height":       desugar(value) })],
+	[/^min-h-(.+)$/,        ([_, value]) => ({ "min-height":   desugar(value) })],
+	[/^max-h-(.+)$/,        ([_, value]) => ({ "max-height":   desugar(value) })],
+	[/^w-(.+)$/,            ([_, value]) => ({ "width":        desugar(value) })],
+	[/^min-w-(.+)$/,        ([_, value]) => ({ "min-width":    desugar(value) })],
+	[/^max-w-(.+)$/,        ([_, value]) => ({ "max-width":    desugar(value) })],
+	[/^aspect-(.+)$/,       ([_, value]) => ({ "aspect-ratio": desugar(value, { px: false }) })],
 
 	/*
 	 * Border-radius
 	 */
-	[/^rounded-(.+)$/,           ([_, value]) => ({ "border-radius":              desugar(value) })],
-	[/^rounded-t-(.+)$/,         ([_, value]) => ({ "border-top-left-radius":     desugar(value), "border-top-right-radius":    desugar(value) })],
-	[/^rounded-r-(.+)$/,         ([_, value]) => ({ "border-top-right-radius":    desugar(value), "border-bottom-right-radius": desugar(value) })],
-	[/^rounded-b-(.+)$/,         ([_, value]) => ({ "border-bottom-left-radius":  desugar(value), "border-bottom-right-radius": desugar(value) })],
-	[/^rounded-l-(.+)$/,         ([_, value]) => ({ "border-bottom-left-radius":  desugar(value), "border-top-right-radius":    desugar(value) })],
-	[/^rounded-tr-(.+)$/,        ([_, value]) => ({ "border-top-right-radius":    desugar(value) })],
-	[/^rounded-br-(.+)$/,        ([_, value]) => ({ "border-bottom-right-radius": desugar(value) })],
-	[/^rounded-bl-(.+)$/,        ([_, value]) => ({ "border-bottom-left-radius":  desugar(value) })],
-	[/^rounded-tl-(.+)$/,        ([_, value]) => ({ "border-top-left-radius":     desugar(value) })],
+	[/^rounded-(.+)$/,      ([_, value]) => ({ "border-radius":              desugar(value) })],
+	[/^rounded-t-(.+)$/,    ([_, value]) => ({ "border-top-left-radius":     desugar(value), "border-top-right-radius":    desugar(value) })],
+	[/^rounded-r-(.+)$/,    ([_, value]) => ({ "border-top-right-radius":    desugar(value), "border-bottom-right-radius": desugar(value) })],
+	[/^rounded-b-(.+)$/,    ([_, value]) => ({ "border-bottom-left-radius":  desugar(value), "border-bottom-right-radius": desugar(value) })],
+	[/^rounded-l-(.+)$/,    ([_, value]) => ({ "border-bottom-left-radius":  desugar(value), "border-top-right-radius":    desugar(value) })],
+	[/^rounded-tr-(.+)$/,   ([_, value]) => ({ "border-top-right-radius":    desugar(value) })],
+	[/^rounded-br-(.+)$/,   ([_, value]) => ({ "border-bottom-right-radius": desugar(value) })],
+	[/^rounded-bl-(.+)$/,   ([_, value]) => ({ "border-bottom-left-radius":  desugar(value) })],
+	[/^rounded-tl-(.+)$/,   ([_, value]) => ({ "border-top-left-radius":     desugar(value) })],
 
 	/*
 	 * Decoration
 	 */
-	[/^color-(.+)$/,             ([_, value]) => ({ "color":            desugar(value, { px: false }) })],
-	[/^bg-color-(.+)$/,          ([_, value]) => ({ "background-color": desugar(value, { px: false }) })],
-	[/^bg-image-(.+)$/,          ([_, value]) => ({ "background-image": desugar(value, { px: false }) })],
-	[/^shadow-(.+)$/,            ([_, value]) => ({ "box-shadow":       desugar(value, { px: false }) })],
+	[/^color-(.+)$/,        ([_, value]) => ({ "color":            desugar(value, { px: false }) })],
+	[/^bg-color-(.+)$/,     ([_, value]) => ({ "background-color": desugar(value, { px: false }) })],
+	[/^bg-image-(.+)$/,     ([_, value]) => ({ "background-image": desugar(value, { px: false }) })],
+	[/^shadow-(.+)$/,       ([_, value]) => ({ "box-shadow":       desugar(value, { px: false }) })],
 ]
 
 ////////////////////////////////////////////////////////////////////////////////
