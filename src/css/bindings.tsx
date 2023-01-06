a1a344ef085758f4ea4fc71da4f6ea305c181cf3
Zaydek <zaydekdotcom@gmail.com>
2023-01-06 04:49:10 -0800

    Tweak

--- src/css/bindings.tsx
+++ src/css/bindings.tsx
@@ -4,13 +4,15 @@ import "./typography.scss"
 
 import { createStyled } from "../lib/react/create-styled"
 
-export const SearchBarContainer        = createStyled("search-bar-container")
-export const SearchBarMask             = createStyled("search-bar-mask")
-export const MainContainer             = createStyled("main-container")
-export const Sidebar1                  = createStyled("sidebar-1")
-export const Sidebar2                  = createStyled("sidebar-2")
-export const Sidebar2Backdrop          = createStyled("sidebar-2-backdrop")
+// Layout
+export const SearchBarContainer       = createStyled("search-bar-container")
+export const SearchBarMask            = createStyled("search-bar-mask")
+export const MainContainer            = createStyled("main-container")
+export const Sidebar1                 = createStyled("sidebar-1")
+export const Sidebar2                 = createStyled("sidebar-2")
+export const Sidebar2Backdrop         = createStyled("sidebar-2-backdrop")
 
+// Form
 export const Radio                    = createStyled("radio")
 export const RadioIcon                = createStyled("radio-icon")
 export const Checkbox                 = createStyled("checkbox")
@@ -19,6 +21,7 @@ export const Slider                   = createStyled("slider")
 export const SliderTrack              = createStyled("slider-track")
 export const SliderThumb              = createStyled("slider-thumb")
 
+// Typography
 export const TypographySearchBar      = createStyled("typography-search-bar")
 export const TypographyGridItemName   = createStyled("typography-grid-item-name")
 export const TypographySans           = createStyled("typography-sans")
