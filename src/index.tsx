053e4aca399ea842c03effae3e6cb76e7d10966a
Zaydek <zaydekdotcom@gmail.com>
2023-01-05 18:27:01 -0800

    More organization

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,12 @@ import "uno.css"
 import "./css/base.scss"
 import "./css/vars.scss"
 
-import ReactDOM from "react-dom/client"
+import { createRoot } from "react-dom/client"
 import { App } from "./app"
 import { ProgressBarProvider } from "./progress-bar"
 import { StateProvider } from "./state"
 
-ReactDOM
-	.createRoot(document.getElementById("root")!)
+createRoot(document.getElementById("root")!)
 	.render(<>
 		<ProgressBarProvider>
 			<StateProvider>
