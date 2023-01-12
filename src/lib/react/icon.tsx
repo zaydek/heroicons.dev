af6a5e6f82ce032405d0c35a950d63089d5217cf
Zaydek <zaydekdotcom@gmail.com>
2023-01-12 13:26:17 -0800

    Add editorconfig

--- src/lib/react/icon.tsx
+++ src/lib/react/icon.tsx
@@ -1,4 +1,4 @@
-import { createElement, SVGAttributes } from "react";
+import { createElement, SVGAttributes } from "react"
 
 export function Icon({ icon, ...props }: { icon: (_: SVGAttributes<SVGSVGElement>) => JSX.Element } & SVGAttributes<SVGSVGElement>) {
   return createElement(icon, props)
